60715d0713214024598039e8003589d3a1a52ce1
wuyanchong <363648823@qq.com>
2020-10-21 16:40:11 +0800

    删除代码

--- com.yg.parent/com.yg.api/src/main/java/com/yg/api/service/menu/YgMenuService.java
+++
@@ -1,9 +0,0 @@
-package com.yg.api.service.menu;
-
-import com.yg.api.result.menu.YgMenuResult;
-
-import java.util.List;
-
-public interface YgMenuService {
-	public List<YgMenuResult> getMenuList(Integer merchantId);
-}
--- com.yg.parent/com.yg.core/src/main/java/com/yg/core/utils/UserConstants.java
+++
@@ -1,6 +0,0 @@
-package com.yg.core.utils;
-
-public final class UserConstants {
-	public static final String ENCRYPT_AUTH_KEY	= "{_ENCRYPT_AUTN_KEY_}";
-	public static final String USER_HEAD_IMAGE_URL = "https://c1.51jujibao.cn/images/default_user.png";
-}
